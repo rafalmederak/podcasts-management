9f63351a7cda1ada94a6217562d10eee3b6036b7
rafalmederak <rafal.mederak1@gmail.com>
2024-07-09 21:05:47 +0200

    PM-12 Add global styles and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import { defaultBeigeColors } from './src/colors/defaultBeige';
+import { defaultBlueColors } from './src/colors/defaultBlue';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/colors/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      screens: {
+        xs: '400px',
+      },
+      colors: {
+        defaultBlue: {
+          ...defaultBlueColors,
+        },
+        defaultBeige: {
+          ...defaultBeigeColors,
+        },
       },
     },
   },
