320e1c431f42be2c055f62c5ac234f25b29a0ee3
rafalmederak <rafal.mederak1@gmail.com>
2024-11-23 12:34:33 +0100

    PM-45 Create Help Page

---
+++ src/app/dashboard/help/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { getFAQ } from '@/services/faq.service';
+import React from 'react';
+import useSWR from 'swr';
+
+const HelpPage = () => {
+  const { data } = useSWR('faq', getFAQ);
+  return (
+    <div className="flex flex-col w-full gap-10 px-4">
+      <h2 className="page__title">FAQ</h2>
+      <div className="flex flex-col gap-6">
+        {data?.map((item) => (
+          <div key={item.id} className="flex flex-col gap-6">
+            <span className="h-0.5 w-full bg-gray-100"></span>
+            <div className="flex flex-col 2xl:flex-row gap-2">
+              <h3 className="w-full 2xl:w-2/5 font-medium">{item.question}</h3>
+              <p className="w-full 2xl:w-3/5 font-light">{item.answer}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HelpPage;
